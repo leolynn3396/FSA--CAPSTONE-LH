3874308e8dd4254a9fc56a9ae651d49abc18e37e
JeremyRogersFSA <jeremy.rogers@fullstackacademy.com>
2024-04-10 17:04:56 -0600

    initialized, main file structure - next: placeholder routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById('root')
+const app = createRoot(root)
+app.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
